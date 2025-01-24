58966f1f1e0608357dbbbf7d458a34da54a8aba8
DonMB77 <purposefuldrifter@gmail.com>
2025-01-24 22:12:19 +0100

    initial init

---
+++ src/main/java/com/drifter/spring_reactive_mongo_sb/SpringReactiveMongoSbApplication.java
@@ -0,0 +1,13 @@
+package com.drifter.spring_reactive_mongo_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveMongoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveMongoSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drifter/spring_reactive_mongo_sb/SpringReactiveMongoSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drifter.spring_reactive_mongo_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveMongoSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
